b33e6ab2c8d08601185ef6d3a947f0cc5fc181c1
dicky0411 <73293265+dicky0411@users.noreply.github.com>
2024-07-24 16:52:27 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,17 +4,15 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProductController; // Ensure ProductController is imported
-
 Route::post('/login', [AuthController::class, 'login'])->name('login');
 Route::post('/register', [AuthController::class, 'register'])->name('register');
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 Route::post('/submit_form', [UserController::class, 'store'])->name('user.store');
 Route::get('/json/{id}', [ProductController::class,'showCountry'])->name('showCountry');
 
-// Correct the routes for ProductController
+
 Route::get('/', [ProductController::class, 'index'])->name('products.index');
 Route::get('/products/{id}', [ProductController::class, 'show'])->name('products.show');
-
 Route::get('/', [ProductController::class, 'index'])->name('home');
 Route::get('/country/{id?}', [ProductController::class, 'showCountry'])->name('country.show');
 Route::get('/region/{code?}', [ProductController::class, 'showRegion'])->name('ip.show');
