73712b08c783db6d7d3c8905eb230234cd45e958
dicky0411 <73293265+dicky0411@users.noreply.github.com>
2024-07-24 13:45:16 +0800

    return API

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Product; // Adjust based on your model namespace
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class ProductController extends Controller
 {
@@ -14,19 +15,12 @@ class ProductController extends Controller
 
         return view('welcome', compact('apis'));
     }
-    public function showCountry($id){
-        $countryAPIUrl = "'http://127.0.0.1:8001/json/'.$id";
-        $response = Country::get($countryAPIUrl);
-        return view('welcome', [
-            'country' => $response->en,
-            'code' => $response->code,
-            'tw' => $response->tw,
-            'locale' => $response->locale,
-            'zh' => $response->zh,
-            'lat' => $response->lat,
-            'lng' => $response->lng,
-            'emoji' => $response->emoji,
-        ]);
+    public function showCountry($id=null){
+        $countryAPIUrl = "http://127.0.0.1:8000/json/$id";
+        $response = Http::get($countryAPIUrl);
+        $data = json_decode($response, true);
+        $pretty_json = json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+        return $pretty_json;
         
 
     }
