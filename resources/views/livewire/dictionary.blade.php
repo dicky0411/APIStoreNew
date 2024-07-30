545b2ee58c934e198de50050e01e47b27addfbee
dicky0411 <73293265+dicky0411@users.noreply.github.com>
2024-07-30 16:55:06 +0800

    dictionary 1

--- resources/views/livewire/dictionary.blade.php
+++ resources/views/livewire/dictionary.blade.php
@@ -1,3 +1,32 @@
 <div>
-    {{-- If your happiness depends on money, you will never be happy with yourself. --}}
+    <br>
+
+    <div class="container mt-5">
+        <h1 class="text-center">Dictionary Defintion</h1>
+        <!-- Livewire component for IP search -->
+        <form id="ipForm" class="mt-4" wire:submit.prevent="search">
+            <div class="form-group">
+                <label for="ipAddress">Type in a word that you are unsure of the definitions of</label>
+                <input type="text" class="form-control" wire:model.defer="word" id="ipAddress" placeholder="apple" required>
+            </div>
+            <button type="submit" class="btn btn-primary btn-block">Search</button>
+        </form>
+        <div id="result" class="mt-4">
+            @if ($definition)
+                <div class="alert alert-success">
+                    @foreach($definition as $line){
+                        {{$line}} <br>
+                    }
+                    @endforeach
+
+                </div>
+            @endif
+
+            @if ($errorMessage)
+                <div class="alert alert-danger">
+                    {{ $errorMessage }}
+                </div>
+            @endif
+        </div>
+    </div>
 </div>
