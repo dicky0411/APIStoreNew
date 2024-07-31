1f89a050e0c14fcc348c228c17fdd9762d7b0ffb
dicky0411 <73293265+dicky0411@users.noreply.github.com>
2024-07-31 10:59:16 +0800

    clean up

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -9,14 +9,86 @@ use Illuminate\Support\Facades\View;
 class ProductController extends Controller
 {
     
-    public function index()
-    {
-        $products = Product::all();
-
-        // TODO: pagination
-
+    
+    public function index() {
+        $products = [
+            [
+                'image' => 'calendar1.webp',
+                'title' => 'Calendar API',
+                'description' => 'Retrieve comprehensive data on holidays across the United States. With the input of a month and a date, the API will return accurate details about the holiday on that date.',
+                'code' => 'calendar'
+            ],
+            [
+                'image' => 'ipRegion.jpg',
+                'title' => 'Regional IP API',
+                'description' => 'Access detailed information about IP addresses, including geolocation data, and the absolute location with the input of a valid IP address.',
+                'code' => 'ip2region'
+            ],
+            [
+                'image' => 'crypto1.png',
+                'title' => 'Cryptocurrency API',
+                'description' => 'Get accurate and up-to-date data on any type of cryptocurrency and convert it to most currencies.',
+                'code' => 'crypto'
+            ],
+            [
+                'image' => 'currentIP.jpeg',
+                'title' => 'Current IP Address API',
+                'description' => 'This is a simple API that returns your current IP Address.',
+                'code' => 'currentIP'
+            ],
+            [
+                'image' => 'currencyexchange.jpeg',
+                'title' => 'Currency Exchange API',
+                'description' => 'This API converts any amount of any currency to its equivalent in USD. It will ask you for the currency type and the amount and it will calculate for you.',
+                'code' => 'Cur-Ex'
+            ],
+            [
+                'image' => 'zipcode.jpeg',
+                'title' => 'Zip Code API',
+                'description' => 'This API takes a 5-digit input as the zip code for the United States, and it will return the location of the zip code, including the locality and the state it is in.',
+                'code' => 'zip-code'
+            ],
+            [
+                'image' => 'shhhh.jpg',
+                'title' => 'Profanity Eraser API',
+                'description' => 'This API can filter out profanities in a large chunk of text, returning each profanity as **** to produce a safe environment.',
+                'code' => 'profanity-eraser'
+            ],
+            [
+                'image' => 'gender.jpeg',
+                'title' => 'Gender Probability API',
+                'description' => 'This API takes a first name or a full name and calculates the probability of the name being that gender.',
+                'code' => 'gender'
+            ],
+            [
+                'image' => 'dictionary.jpeg',
+                'title' => 'Dictionary API',
+                'description' => 'This API returns every definition of a word listed from 1 to n, and works for every English word.',
+                'code' => 'dictionary'
+            ],
+            [
+                'image' => 'fruits.jpeg',
+                'title' => 'Fruit Nutrition API',
+                'description' => 'This API outputs all the macro and micro nutrition data of all fruits, from domestic to tropical fruits. Data displayed includes calories, proteins, and more.',
+                'code' => 'fruits'
+            ],
+            [
+                'image' => 'advancedIP.jpeg',
+                'title' => 'Advanced IP API',
+                'description' => 'This API outputs the longitude and latitude of the IP entered, and also replies with the area code for the user\'s convenience.',
+                'code' => 'advanced-ip'
+            ],
+            [
+                'image' => 'covid.jpeg',
+                'title' => 'COVID Cases API',
+                'description' => 'This API outputs the total cases at a specific date in a specific state in the United States in 2020, showcasing how fast COVID spreads.',
+                'code' => 'covid'
+            ],
+        ];
+    
         return view('product.index', compact('products'));
     }
+    
 
     public function show($code)
     {
@@ -41,8 +113,4 @@ class ProductController extends Controller
 
         return abort(404);
     }
-
-   
-    
-   
 }
--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -2,167 +2,19 @@
 
 @section('content')
     <div class="container mt-5">
-        <div class="row">
-            <div class="col">
-                <div class="card" style="width: 18rem;">
-                    <img src="images/calendar1.webp" class="card-img-top" alt="Calendar API">
-                    <div class="card-body">
-                        <h5 class="card-title">Calendar API</h5>
-                        <p class="card-text">Retrieve comprehensive data on holiday across united states, with the input of a 
-                                month and a date, the API will return the accurdate detail about the holiday on that date
-                        </p>
-                        <a href="{{ route('products.show', ['code' => 'calendar']) }}"class="btn btn-primary">Enter</a>
+        <div class="row row-cols-1 row-cols-md-3 g-4">
+            @foreach ($products as $product)
+                <div class="col">
+                    <div class="card h-100">
+                        <img src="images/{{ $product['image'] }}" class="card-img-top" alt="{{ $product['title'] }}">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $product['title'] }}</h5>
+                            <p class="card-text">{{ $product['description'] }}</p>
+                            <a href="{{ route('products.show', ['code' => $product['code']]) }}" class="btn btn-primary">Enter</a>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="col">
-                <div class="card" style="width: 18rem;">
-                    <img src="images/ipRegion.jpg" class="card-img-top" alt="IP Geolocation">
-                    <div class="card-body">
-                        <h5 class="card-title">Regional IP API</h5>
-                        <p class="card-text">Access detailed information about IP addresses, including geolocation data, and the absolute location of the location with the input of a valid IP address.</p>
-                        <a href="{{ route('products.show', ['code' => 'ip2region']) }}"
-                                class="btn btn-primary">Enter</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="card" style="width: 18rem;">
-                    <img src="images/crypto1.png" class="card-img-top" alt="Crypto">
-                    <div class="card-body">
-                        <h5 class="card-title">Cryptocurrency API</h5>
-                        <p class="card-text">Get accurate and up-to-date data on any type of cryptocurrency and can be converted to most currencies.</p>
-                        <br><br>
-                    
-                        <a href="{{ route('products.show', ['code' => 'crypto']) }}"
-                            class="btn btn-primary">Enter</a>
-                    </div>
-                </div>
-            </div>
+            @endforeach
         </div>
-        <div class="row">
-            <div class="col">
-            
-                <div class="card" style="width: 18rem;">
-                    <img src="images/currentIP.jpeg" class="card-img-top" alt="currentIP">
-                    <div class="card-body">
-                        <h5 class="card-title">Current IP Address API</h5>
-                        <p class="card-text">This is a simple API that returns your current IP Address</p>
-                        
-                        <a href="{{ route('products.show', ['code' => 'currentIP']) }}"
-                            class="btn btn-primary">Enter</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col">
-            
-                <div class="card" style="width: 18rem;">
-                    <img src="images/currencyexchange.jpeg" class="card-img-top" alt="curex">
-                    <div class="card-body">
-                        <h5 class="card-title">Currency Exchange API</h5>
-                        <p class="card-text">This API converts any number of any currency to its equivalent in USD, it'll ask you for the currency type and the amount and it'll calculate for you</p>
-                        
-                        <a href="{{ route('products.show', ['code' => 'Cur-Ex']) }}"
-                            class="btn btn-primary">Enter</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col">
-            
-                <div class="card" style="width: 18rem;">
-                    <img src="images/zipcode.jpeg" class="card-img-top" alt="zip">
-                    <div class="card-body">
-                        <h5 class="card-title">Zip Code API</h5>
-                        <p class="card-text">This API takes an 5 digit input as the zip code for united states, and it'll return the location of the zip code, including the local and the state that its in</p>
-                        
-                        <a href="{{ route('products.show', ['code' => 'zip-code']) }}"
-                            class="btn btn-primary">Enter</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col">
-            
-                <div class="card" style="width: 18rem;">
-                    <img src="images/shhhh.jpg" class="card-img-top" alt="shhhh">
-                    <div class="card-body">
-                        <h5 class="card-title">Profanity Eraser API</h5>
-                        <ter class="card-text">This API is a simple, it has the ability to filter out profanities in a large chunk of text, it returns each profanity in ****, it'll produce a safe environment</p>
-                        
-                        <a href="{{ route('products.show', ['code' => 'profanity-eraser']) }}"
-                            class="btn btn-primary">Enter</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col">
-           
-                <div class="card" style="width: 18rem;">
-                    <img src="images/gender.jpeg" class="card-img-top" alt="gender">
-                    <div class="card-body">
-                        <h5 class="card-title">Gender Probability API</h5>
-                        <ter class="card-text">This API takes a first name or a full name and calculates the Probability of the name being that gender</p>
-                        
-                        <a href="{{ route('products.show', ['code' => 'gender']) }}"
-                            class="btn btn-primary">Enter</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col">
-            
-                <div class="card" style="width: 18rem;">
-                    <img src="images/dictionary.jpeg" class="card-img-top" alt="dict">
-                    <div class="card-body">
-                        <h5 class="card-title">Dictionary API</h5>
-                        <ter class="card-text">This API returns every definition of a word listed from 1 to n, works for every english word</p>
-                        
-                        <a href="{{ route('products.show', ['code' => 'dictionary']) }}"
-                            class="btn btn-primary">Enter</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col">
-            
-                <div class="card" style="width: 18rem;">
-                    <img src="images/fruits.jpeg" class="card-img-top" alt="shhhh">
-                    <div class="card-body">
-                        <h5 class="card-title">Fruit Nutrition API</h5>
-                        <ter class="card-text">This API outputs all the macro and micro nutrition data of all fruits, from domestic to tropical fruits, data displayed includes calories, proteins and more</p>
-                        
-                        <a href="{{ route('products.show', ['code' => 'fruits']) }}"
-                            class="btn btn-primary">Enter</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col">
-            
-                <div class="card" style="width: 18rem;">
-                    <img src="images/advancedIP.jpeg" class="card-img-top" alt="Advanced IP">
-                    <div class="card-body">
-                        <h5 class="card-title">Advanced IP API</h5>
-                        <ter class="card-text">This API outputs the Longitude and Latitude of the IP entered, and also replies with the area code for the user's convenience</p>
-                        
-                        <a href="{{ route('products.show', ['code' => 'advanced-ip']) }}"
-                            class="btn btn-primary">Enter</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col">
-            
-                <div class="card" style="width: 18rem;">
-                    <img src="images/covid.jpeg" class="card-img-top" alt="covid">
-                    <div class="card-body">
-                        <h5 class="card-title">COVID Cases API</h5>
-                        <ter class="card-text">This API outputs the total cases at xxxx date at xxx state in the United States in 2020, it showcases how fast covid spreads</p>
-                        
-                        <a href="{{ route('products.show', ['code' => 'covid']) }}"
-                            class="btn btn-primary">Enter</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-     </div>
-   
+    </div>
 @endsection
