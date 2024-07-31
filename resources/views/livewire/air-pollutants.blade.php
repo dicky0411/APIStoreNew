9ba6b65a5b11e05ff204199526bf189378cdde45
dicky0411 <73293265+dicky0411@users.noreply.github.com>
2024-07-31 16:20:09 +0800

    pollutants API

--- resources/views/livewire/air-pollutants.blade.php
+++ resources/views/livewire/air-pollutants.blade.php
@@ -1,3 +1,36 @@
 <div>
-    {{-- To attain knowledge, add things every day; To attain wisdom, subtract things every day. --}}
+    <br>
+
+    <div class="container mt-5">
+        <h1 class="text-center">Pollution Metrics API</h1>
+        
+        
+        <form id="calendarForm" class="mt-4" wire:submit.prevent="search">
+            <div class="form-group">
+                <label for="AQI">What city would you like to choose?</label>
+                <input type="text" class="form-control" wire:model.defer="city" id="city" placeholder="Ex: Toronto" required>
+                
+            </div>
+            <button type="submit" class="btn btn-primary btn-block">Search</button>
+        </form>
+        <div id="result" class="mt-4">
+            
+            @if ($o3 && $pm10 && $pm25 &&$uvi)
+                <div class="alert alert-success">
+                    In {{$city}}, the average pollution metrics of today are the following: <br>
+                    O3 (Ozone): {{$o3}} mg/l <br>
+                    PM10 (Particulate Matter 10): {{$pm10}} μg/m³ <br>
+                    PM25 (Particulate Matter 25): {{$pm25}} μg/m³ <br>
+                    UVI (Ultraviolet Index): {{$uvi}}
+
+                </div>
+            @endif
+
+            @if ($errorMessage)
+                <div class="alert alert-danger">
+                    {{ $errorMessage }}
+                </div>
+            @endif
+        </div>
+    </div>
 </div>
